fd02609170de548e534576ee79da8756ceb06c42
samiTab <sami.tabibel@gmail.com>
2019-12-15 21:10:15 +0100

    parking explorer: first commit

---
+++ client/ServiceRequest.js
@@ -0,0 +1,28 @@
+
+class ServiceRequest {
+
+  // build ServiceRequest to perform requests to the source api
+  constructor(options, id) {
+    this.sr = {
+      ...options,
+    };
+    
+    this.recordid = id;
+  }
+
+  // get all records: no filter is applied.
+  getAll() {
+    const { ...srCopy } = this.sr;
+    return srCopy;
+  }
+  /**
+   * retrieve only the specified record
+   */
+  getByRecordID() {
+    const { ...srCopy } = this.sr;
+    srCopy.uri = srCopy.uri+`&refine.recordid=${this.recordid}`;
+    return srCopy;
+  }
+}
+
+module.exports = ServiceRequest;
